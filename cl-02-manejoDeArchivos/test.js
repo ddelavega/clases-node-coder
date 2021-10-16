83c2fa0e67551496934b02d559824180ac386a42
Diego de la Vega <ddelavega@MacBook-Pro-Chucky.local>
2021-10-16 16:56:23 -0300

    Clases Coder

--- cl-02-manejoDeArchivos/test.js
+++ cl-02-manejoDeArchivos/test.js
@@ -1,6 +1,6 @@
 const Contenedor = require('./main');
 
-const productosDB = new Contenedor("productos");
+const productosDB = new Contenedor("productos.json");
 
 // PRUEBAS: comentar o descomentar la segunda linea para probar el metodo
 // yarn serve se ejecuta el script con nodemon
@@ -13,10 +13,10 @@ const test = async () => {
   
   // getById(Number) recibe un id y devuelve el objeto con ese id, o null si no esta.
   // console.log(await productosDB.getById(80));
-//   console.log(await productosDB.getById(6));
+  // console.log(await productosDB.getById(6));
   
   // getAll() devuelve un array con los objetos presentes en el archivo.
-//   console.log(await productosDB.getAll());
+  // console.log(await productosDB.getAll());
   
   // deleteById(Number) elimina del archivo el objeto con el id buscado.
   // console.log(await productosDB.deleteById(8));
