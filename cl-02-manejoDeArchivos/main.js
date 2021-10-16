eff92188f2b2ea45bfed4548a1d49b655943ea9d
Diego de la Vega <ddelavega@MacBook-Pro-Chucky.local>
2021-10-16 16:59:41 -0300

    Clases Coder

--- cl-02-manejoDeArchivos/main.js
+++ cl-02-manejoDeArchivos/main.js
@@ -59,13 +59,12 @@ class Contenedor {
     // await console.log('total de Productos',restoProductos);
     // productById ? console.log('se borró', productById): console.log('NO se borró el producto o no existe');
     await fs.promises.writeFile(this.nombreArchivo,JSON.stringify(restoProductos,null,2));
-      return productById? console.log('Se borró el producto con ID:', id): console.log('No se encuentra el producto con ID:',id);
+    return productById? console.log('Se borró el producto con ID:', id): console.log('No se encuentra el producto con ID:',id);
   }
 
   deleteAll = async () => {
     const productos = await this.getAll();
     if(productos.length) {
-
       await fs.promises.writeFile(this.nombreArchivo,'');
       productos ? console.log('Se borraron todos los productos!'): productos;
     }
@@ -74,29 +73,3 @@ class Contenedor {
 }
 
 module.exports = Contenedor;
-// const productosDB = new Contenedor("productos");
-
-// // PRUEBAS: comentar o descomentar la segunda linea para probar el metodo
-// // yarn serve se ejecuta el script con nodemon
-// // yarn start se ejecuta el script con node
-
-// const test = async () => {
-
-//   // save(Object) recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-//   // console.log("Guarda Producto", productosDB.save({title: "Hot Wheels", price: 30, thumbnail: "http://algo.com"}));
-  
-//   // getById(Number) recibe un id y devuelve el objeto con ese id, o null si no esta.
-//   // console.log(await productosDB.getById(80));
-//   // console.log(await productosDB.getById(8));
-  
-//   // getAll() devuelve un array con los objetos presentes en el archivo.
-//   console.log(await productosDB.getAll());
-  
-//   // deleteById(Number) elimina del archivo el objeto con el id buscado.
-//   // console.log(await productosDB.deleteById(8));
-  
-//   // deleteAll(Number) elimina todos los objetos presentes en el archivo.
-//   // console.log(await productosDB.deleteAll());
-// }
-
-// test();
