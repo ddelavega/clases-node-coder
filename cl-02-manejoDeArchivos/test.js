7ffe5e664387b5a8820198e390ec72f824ee440f
Diego de la Vega <ddelavega@MacBook-Pro-Chucky.local>
2021-10-16 16:58:34 -0300

    Clases Coder

--- cl-02-manejoDeArchivos/test.js
+++ cl-02-manejoDeArchivos/test.js
@@ -2,18 +2,18 @@ const Contenedor = require('./main');
 
 const productosDB = new Contenedor("productos.json");
 
-// PRUEBAS: comentar o descomentar la segunda linea para probar el metodo
+// PRUEBAS: comentar o descomentar las lineas que correspondan para probar el metodo
 // yarn serve se ejecuta el script con nodemon
 // yarn start se ejecuta el script con node
 
 const test = async () => {
 
   // save(Object) recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-//   console.log("Guarda Producto", productosDB.save({title: "Hot Wheels", price: 30, thumbnail: "http://algo.com"}));
+  // console.log("Guarda Producto", productosDB.save({title: "Hot Wheels", price: 30, thumbnail: "http://algo.com"}));
   
   // getById(Number) recibe un id y devuelve el objeto con ese id, o null si no esta.
   // console.log(await productosDB.getById(80));
-  // console.log(await productosDB.getById(6));
+  // console.log(await productosDB.getById(2));
   
   // getAll() devuelve un array con los objetos presentes en el archivo.
   // console.log(await productosDB.getAll());
