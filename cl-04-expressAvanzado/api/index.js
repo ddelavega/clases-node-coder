09de898ed1f0f75ec74d18aefbf5a5658df6409d
Diego de la Vega <ddelavega@MacBook-Pro-Chucky.local>
2021-10-25 19:16:02 -0300

    Âajustes entorno publico con vercel

--- cl-04-expressAvanzado/api/index.js
+++ cl-04-expressAvanzado/api/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + './../public'));
 
 const port = process.env.PORT || 8080;
 // const router = Router();
@@ -18,3 +18,5 @@ const server = app.listen(port, () => {
   console.log(`Escuchando al puerto: ${port} en http://localhost:${port}`);
 });
 server.on('error', error => console.log(`Error en servidor${error}`));
+
+module.exports = server;
